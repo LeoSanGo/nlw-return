332f093a77c0fa3b7ab58dc7a9bd605508cd9613
leosango <souoleo@gmail.com>
2022-05-06 17:39:49 -0300

    feat: add new components and its logic

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
 import { FeedbackContentSteps } from './Steps/FeedbackContentSteps';
+import { FeedbackSuccessSteps } from './Steps/FeedbackSuccessSteps';
 
 export const feedbackTypes = {
   BUG: {
@@ -36,23 +37,33 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false)
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />//TODO: - try undestand components communications - 2 -> 56:26
+      {feedbackSent ? (
+        <FeedbackSuccessSteps onFeedbackRestartRequest={handleRestartFeedback}/>
       ) : (
-        <FeedbackContentSteps feedbackType={feedbackType} onFeedbackRestartRequested={handleRestartFeedback}/>
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} /> //TODO: - try undestand components communications - 2 -> 56:26
+          ) : (
+            <FeedbackContentSteps
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ por {' '} 
+        Feito com ♥ por{' '}
         <a
           className="underline underline-offset-2"
           href="https://github.com/leosango"
