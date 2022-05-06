c7e87604d7be9a5b84285afdf333c373ae5ea0b8
leosango <souoleo@gmail.com>
2022-05-06 12:24:29 -0300

    feat: application of SOLID concepts on componets

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+import { FeedbackContentSteps } from './Steps/FeedbackContentSteps';
 
 export const feedbackTypes = {
   BUG: {
@@ -35,21 +36,23 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
+
 
       {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />//TODO: - try undestand components communications - 2 -> 56:26
       ) : (
-        <p>Hello world</p>
+        <FeedbackContentSteps feedbackType={feedbackType} onFeedbackRestartRequested={handleRestartFeedback}/>
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ por{' '}
+        Feito com ♥ por {' '} 
         <a
           className="underline underline-offset-2"
           href="https://github.com/leosango"
