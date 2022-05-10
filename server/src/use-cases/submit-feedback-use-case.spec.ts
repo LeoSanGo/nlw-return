3f95e0ca88564e37f85d0b406f6f3c3f949a7a37
leosango <souoleo@gmail.com>
2022-05-09 21:32:52 -0300

    feat: setup enviromment and add new components and libs

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,34 +1,44 @@
-import{SubmitFeedbackUseCase}from "./submit-feedback-use-case";
+import { SubmitFeedbackUseCase } from './submit-feedback-use-case';
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
 
 const submitFeedback = new SubmitFeedbackUseCase(
-  {create: async () => {} },
-  {sendMail: async () => {}}
-)
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
+);
 
 describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
-    
-    await expect(submitFeedback.execute({
-      type: 'BUG',
-      comment: 'example comment',
-      screenshot: 'data:image/png; base64,sassassacvx',
-     })).resolves.not.toThrow();
+    await expect(
+      submitFeedback.execute({
+        type: 'BUG',
+        comment: 'example comment',
+        screenshot: 'data:image/png; base64,sassassacvx',
+      })
+    ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
   });
 
   it('should not be able to submit feedback without type', async () => {
-    await expect(submitFeedback.execute({
-      type: '',
-      comment: 'example comment',
-      screenshot: 'data:image/png;base64,sassassacvx',
-    })).rejects.toThrow();
+    await expect(
+      submitFeedback.execute({
+        type: '',
+        comment: 'example comment',
+        screenshot: 'data:image/png;base64,sassassacvx',
+      })
+    ).rejects.toThrow();
   });
 
   it('should not be able to submit feedback with an invalid screenshot', async () => {
-    await expect(submitFeedback.execute({
-      type: 'BUG',
-      comment: 'ta tudo bugado',
-      screenshot: 'test.jpg',
-    })).rejects.toThrow();
+    await expect(
+      submitFeedback.execute({
+        type: 'BUG',
+        comment: 'ta tudo bugado',
+        screenshot: 'test.jpg',
+      })
+    ).rejects.toThrow();
   });
-
 });
